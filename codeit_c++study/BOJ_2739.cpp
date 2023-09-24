0df9ee1f75d83d43d3c0e0cc3e024a72134db0f5
Shin Yejin <yejin0605@sookmyung.ac.kr>
2023-09-24 22:16:28 +0900

    BOJ_2739

---
+++ codeit_c++study/BOJ_2739.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	int num1;
+
+	cin >> num1;
+	for (int i=1; i < 10; i++) {
+		cout << num1 << " * " << i << " = " << num1 * i << endl;
+
+	}
+}
